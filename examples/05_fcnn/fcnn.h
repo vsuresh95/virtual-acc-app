af5322dd98d75ee9d42153f2e8cdbe2ace9e7a08
vv15 <vv15@illinois.edu>
2025-08-20 11:05:59 -0500

    Added model ID for FCNN 05

--- examples/05_fcnn/fcnn.h
+++ examples/05_fcnn/fcnn.h
@@ -7,7 +7,7 @@ void fcnn_worker(unsigned worker_id) {
     HIGH_DEBUG(printf("[WORKER%d] Starting worker %d!\n", worker_id, worker_id);)
 
     // Load a model from a text file (and) register with NN frontend
-    nn_module m;
+    nn_module m(0);
     m.load_and_register("model.txt");
 
     // Check if the input flag is set; else write inputs
