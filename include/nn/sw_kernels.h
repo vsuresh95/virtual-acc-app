d622e8d83ffc8ef84653149ac5178fe84d3aa304
vv15 <vv15@illinois.edu>
2025-08-27 16:14:57 -0500

    Moved all CPP files to cpp_backup

--- include/nn/sw_kernels.h
+++
@@ -1,8 +0,0 @@
-#ifndef __SW_KERNELS_H__
-#define __SW_KERNELS_H__
-
-#include <nn_helper.h>
-
-void tiled_gemm(const nn_token_t* A, const nn_token_t* B, nn_token_t* C, unsigned m, unsigned n, unsigned k);
-
-#endif // __SW_KERNELS_H__
